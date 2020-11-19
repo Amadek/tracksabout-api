e7d8456c2a7e47386123d19159b2ba5442122651
Amadeusz Pęcherczyk <apecherczyk@protonmail.com>
2020-11-19 19:11:46 +0100

    Dodanie prawdziwej relacji pliku audio do tracka.

--- src/entities/DbCreator.js
+++ src/entities/DbCreator.js
@@ -2,21 +2,23 @@ const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 const homedir = require('os').homedir();
-const GridFsBucket = require('mongodb').GridFSBucket;
+const mongodb = require('mongodb');
+const GridFsBucket = mongodb.GridFSBucket;
+const ObjectID = mongodb.ObjectID;
 
 module.exports = class DbCreator {
   static artists = [
     {
       name: 'Yes',
       albums: [
-        { name: 'The Yes Album' },
-        { name: 'Close To The Edge' }
-      ]
-    },
-    {
-      name: 'Queen',
-      albums: [
-        { name: 'The Miracle' }
+        {
+          name: 'Close to the Edge',
+          tracks: [
+            { name: 'Close to the Edge' },
+            { name: 'And You and I' },
+            { name: 'Siberian Khatru' }
+          ]
+        },
       ]
     }
   ];
@@ -27,25 +29,51 @@ module.exports = class DbCreator {
   }
 
   create () {
+    console.log('DbCreator.create() started.')
+
     return Promise.resolve()
-      .then(() => console.log('DbCreator.create() started.'))
+      .then(() => this._uploadTrack(path.join(homedir, 'jk.wav')))
+      .then(fileId => DbCreator.artists[0].albums[0].tracks[0].fileId = fileId)
       .then(() => this._db.collection('artists').deleteMany({}))
       .then(() => this._db.collection('artists').insertMany(DbCreator.artists))
-      .then(() => this._uploadTrack(path.join(homedir, 'jk.wav'))) // IT WORKS BUT IT'S SLOW :(
-      .then(() => console.log('DbCreator.create() completed.'))
+      .then(() => console.log("DbCreator.create() completed."))
       .then(() => this._db);
   }
 
+  /**
+   * Uploads file to mongo and returns fileId in promise.
+   * @param {string} trackPath 
+   * @param {string} trackFileName 
+   */
   _uploadTrack (trackPath) {
     const gridFsBucket = new GridFsBucket(this._db, { chunkSizeBytes: 1024, bucketName: 'tracks' });
+    const trackFileName = new ObjectID().toHexString() + path.extname(trackPath);
 
     return new Promise((resolve, reject) => {
       gridFsBucket.drop(() => {
-        fs.createReadStream(trackPath)
-          .pipe(gridFsBucket.openUploadStream(path.basename(trackPath)))
+        const readStream = fs.createReadStream(trackPath).pipe(gridFsBucket.openUploadStream(trackFileName));
+        readStream
           .on('error', error => reject(error))
-          .on('finish', () => resolve());
+          .on('finish', () => {
+            console.log(`File ${readStream.id} uploaded`);
+            resolve(readStream.id);
+          });
       });
     });
   }
 }
+
+// Script to join artists and track files (not perfect yet)
+// db.artists.aggregate([
+//   { $unwind: "$albums" }, { $unwind: "$albums.tracks" },
+//   {
+//     $lookup:
+//     {
+//       from: 'tracks.files',
+//       localField: 'albums.tracks.fileId',
+//       foreignField: '_id',
+//       as: 'tracksWithFiles'
+//     }
+//   },
+//   { $unwind: '$tracksWithFiles' }
+// ]).pretty()
