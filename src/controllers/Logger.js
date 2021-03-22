42746785194868f5db2b5be68fdf2da5355cd7f6
Amadeusz Pęcherczyk <apecherczyk@protonmail.com>
2021-03-22 19:52:31 +0100

    Customowa obsługa błędów w Express.

--- src/controllers/Logger.js
+++ src/controllers/Logger.js
@@ -1,12 +1,19 @@
 const debug = require('debug');
+const assert = require('assert');
 
 module.exports = class Logger {
-  /**
-   * @param {object} sender
-   * @param {any} message
-   */
   log (sender, message) {
-    if (!this._debugScope) this._debugScope = debug('tracksabout-api:' + sender.constructor.name);
+    assert.ok(sender);
+    assert.ok(message);
+
+    if (!this._debugScope) this._debugScope = debug('tracksabout-api:' + this._getSenderName(sender));
     this._debugScope(message);
+  }
+
+  _getSenderName (sender) {
+    switch (typeof sender) {
+      case 'object': return sender.constructor.name;
+      case 'string': return sender;
+    }
   }
 };
--- src/index.js
+++ src/index.js
@@ -14,11 +14,16 @@ Promise.resolve()
   .then(db => {
     const app = express();
     app.use(express.json());
+    const logger = new Logger();
     const trackParser = new TrackParser(new Logger());
     const trackUploader = new TrackUploader(db, new Logger());
     const artistHierarchyUpdater = new AritstHierarchyUpdater(db, new Logger());
     app.use('/track', new TrackController(trackParser, trackUploader, artistHierarchyUpdater, new Logger()).route());
     // Any other route should throw Not Found.
     app.use((_req, _res, next) => next(new NotFound()));
-    app.listen(3000, () => console.log(`Listening on ${3000}...`));
+    app.use((err, _req, res, _next) => {
+      logger.log('index', err);
+      res.status(err.status).send(err.stack);
+    });
+    app.listen(3000, () => logger.log('index', `Listening on ${3000}...`));
   });
