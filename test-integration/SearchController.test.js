01060c17355fe988283b01e378ac6d7d9c716512
Amadeusz Pęcherczyk <apecherczyk@protonmail.com>
2021-09-12 15:49:32 +0200

    Przywrócenie 5s timoeutu w teście.

--- test-integration/SearchController.test.js
+++ test-integration/SearchController.test.js
@@ -323,7 +323,7 @@ describe(SearchController.name, () => {
       assert.strictEqual(searchByIdResult.albums[0].name, trackBaseData.albumName);
       assert.ok(searchByIdResult.albums[0].year);
       assert.strictEqual(searchByIdResult.albums[0].tracks, undefined, "we don't want to send tracks of artist's album, only album's data");
-    });
+    }).timeout(5000);
   });
 });
 
